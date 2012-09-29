e23759ae2beec654a2e42e446f0dba3989b3c662
psychocandy <github@null.co.il>
2012-09-29 16:28:05 +0200

    pretty working little gem

--- lib/quotey/quoter.rb
+++ lib/quotey/quoter.rb
@@ -3,18 +3,20 @@ module Quotey
   class NoDataFile < StandardError; end
 
   class Quoter
-    attr_reader :data
+    attr_reader :data, :size
 
-    def initialize(quotes_source = "misc_quotes.txt", options = {})
-      if File.exist? quotes_source
-        @data = File.read()
+    def initialize(quotes_source = nil, options = {})
+      @source_file = quotes_source.nil? ? File.join(File.dirname(__FILE__), 'misc_quotes.txt') : quotes_source
+      if File.exist? @source_file
+        @data = File.open(@source_file,"r").readlines.map{ |line| line.chomp }
+        @size = @data.size
       else
         raise NoDataFile, "Cannot find quotes data file - #{quotes_source}"
       end
     end
 
     def get_quote
-
+      @data[rand(@size)]
     end
   end
 
--- spec/quotey_spec.rb
+++ spec/quotey_spec.rb
@@ -2,20 +2,39 @@ require 'Quotey'
 
 describe Quotey::Quoter do
 
-  # it { should respond_to(:get_quote) }
-  # it { should respond_to(:data) }
 
-  let(:quoter) { Quotey::Quoter.new }
+  describe "default behavior" do
+    let(:quoter) { Quotey::Quoter.new }
+    it "raises NoDataFile exception if a non-existent file is given" do
+      file = ('a'..'z').to_a.shuffle[0,10].join("") << ".txt"
+      expect {
+        Quotey::Quoter.new(file)
+      }.to raise_error Quotey::NoDataFile
+    end
 
-  it "returns a quote" do
-    quoter.get_quote.should_not be_nil
+    it "reads the default file" do
+      default_path = local_file = File.join(File.dirname(__FILE__), '..', 'lib', 'quotey', 'misc_quotes.txt')
+      size = File.open(default_path,"r").readlines.count
+      quoter.size.should == size
+    end
   end
 
-  it "raises NoDataFile exception if a non-existent file is given" do
-    file = ('a'..'z').to_a.shuffle[0,10].join("") << ".txt"
-    expect {
-      Quotey::Quoter.new(file)
-    }.to raise_error Quotey::NoDataFile
+  context "when custom file is given" do
+    before(:all) do
+      @local_path = File.join(File.dirname(__FILE__), 'misc_quotes.txt')
+      @quoter = Quotey::Quoter.new @local_path
+    end
+
+    it "reads all the quotes from a custom file" do
+      size = File.open(@local_path,"r").readlines.count
+      @quoter.size.should == size
+    end
+
+    describe "#get_quote" do
+      it "returns a quote" do
+        @quoter.get_quote.should_not be_nil
+      end
+    end
   end
 
 
