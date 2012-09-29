2c78fa1e6a69e3742a4e3a448b0295a144c73290
psychocandy <github@null.co.il>
2012-09-29 16:47:39 +0200

    adding a no_repeat mode

--- lib/quotey/quoter.rb
+++ lib/quotey/quoter.rb
@@ -3,21 +3,34 @@ module Quotey
   class NoDataFile < StandardError; end
 
   class Quoter
-    attr_reader :data, :size
+    attr_reader :data, :size, :no_repeat
 
     def initialize(quotes_source = nil, options = {})
-      @source_file = quotes_source.nil? ? File.join(File.dirname(__FILE__), 'misc_quotes.txt') : quotes_source
+      @source_file = quotes_source.nil? || quotes_source == :default_file ? File.join(File.dirname(__FILE__), 'misc_quotes.txt') : quotes_source
       if File.exist? @source_file
         @data = File.open(@source_file,"r").readlines.map{ |line| line.chomp }
         @size = @data.size
+        set_options(options)
       else
         raise NoDataFile, "Cannot find quotes data file - #{quotes_source}"
       end
     end
 
     def get_quote
-      @data[rand(@size)]
+      if @no_repeat
+        index = rand(@size)
+        @size -= 1
+        @data.delete_at index
+      else
+        @data[rand(@size)]
+      end
     end
+    
+  private
+    def set_options(options)
+      @no_repeat = options[:no_repeat] ? true : false
+    end
+
   end
 
 end
--- spec/quotey_spec.rb
+++ spec/quotey_spec.rb
@@ -3,7 +3,7 @@ require 'Quotey'
 describe Quotey::Quoter do
 
 
-  describe "default behavior" do
+  describe "basic behavior" do
     let(:quoter) { Quotey::Quoter.new }
     it "raises NoDataFile exception if a non-existent file is given" do
       file = ('a'..'z').to_a.shuffle[0,10].join("") << ".txt"
@@ -16,6 +16,32 @@ describe Quotey::Quoter do
       default_path = local_file = File.join(File.dirname(__FILE__), '..', 'lib', 'quotey', 'misc_quotes.txt')
       size = File.open(default_path,"r").readlines.count
       quoter.size.should == size
+    end
+
+    it "does not set no_repeat to true" do
+      quoter.no_repeat.should_not be_true
+    end
+  end
+
+  context "when no_repeat is given" do
+    let(:quoter) { Quotey::Quoter.new(:default_file, no_repeat: true) }
+
+    it "sets no_repeat to true" do
+      quoter.no_repeat.should be_true
+    end
+
+    it "deletes the given quote from the list" do
+      deleted_quote = quoter.get_quote
+      quoter.data.include?(deleted_quote).should be_false
+    end
+
+    it "lowers the size by 1" do
+      expect{ quoter.get_quote }.to change{ quoter.size }.by(-1)
+    end
+
+    it "#get_quote returns a quote" do
+      quoter.get_quote.should_not be_nil
+      quoter.get_quote.class.should == String
     end
   end
 
@@ -30,10 +56,9 @@ describe Quotey::Quoter do
       @quoter.size.should == size
     end
 
-    describe "#get_quote" do
-      it "returns a quote" do
-        @quoter.get_quote.should_not be_nil
-      end
+    it "#get_quote returns a quote" do
+      @quoter.get_quote.should_not be_nil
+      @quoter.get_quote.class.should == String
     end
   end
 
